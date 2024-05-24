3c7df169a1fb62d02ddd07f44b7d3d69b0b0899d
InnaTheDeveloper <93871719+InnaTheDeveloper@users.noreply.github.com>
2024-05-24 16:16:57 +0300

    project upload

---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus ipsam
+        sapiente officiis, quibusdam aspernatur dicta id vero similique
+        repellendus, amet fugiat, assumenda porro voluptates nostrum officia
+        cumque ipsum repudiandae! Necessitatibus ullam aliquid, voluptatum
+        repellendus officiis deserunt sint ratione aperiam aut.
+      </p>
+    </section>
+  );
+};
+
+export default About;
